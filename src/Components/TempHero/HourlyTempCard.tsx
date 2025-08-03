81fe0ce2eaa4e4efbad61f23ad4e3876a0383765
erredev-JS <rodrigozapata968@gmail.com>
2025-08-02 23:15:06 -0300

    Fix css

--- src/Components/TempHero/HourlyTempCard.tsx
+++ src/Components/TempHero/HourlyTempCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 export const HourlyTempCard: FC<Props> = ({ hourlyTemp }) => {
   return (
-    <div className="flex flex-col  px-2 py-1 border w-1/4 rounded-2xl max-h-[85px] max-w-[100px] ">
+<div className="flex flex-col items-center justify-center  py-1 border rounded-2xl w-[22vw] max-w-[70px] min-w-[50px] max-h-[85px] text-xs sm:text-sm md:text-base">
       <span >{hourlyTemp.hour}</span>
       <span >{hourlyTemp.temp}º</span>
     </div>
